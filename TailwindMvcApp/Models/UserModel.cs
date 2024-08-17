7d928fc5779d861fd84196eec34aee9563967375
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-08-18 02:53:04 +0630

    feat: user model

---
+++ TailwindMvcApp/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TailwindMvcApp.Models
+{
+    public class UserModel
+    {
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
