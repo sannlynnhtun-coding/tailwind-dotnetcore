e9891d475460ea204d49f9f7520fb5541a8fe5ff
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-08-18 02:54:05 +0630

    feat: add services

--- TailwindMvcApp/Program.cs
+++ TailwindMvcApp/Program.cs
@@ -1,4 +1,43 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using TailwindMvcApp.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// If using Kestrel:
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.AllowSynchronousIO = true;
+});
+
+// If using IIS:
+builder.Services.Configure<IISServerOptions>(options =>
+{
+    options.AllowSynchronousIO = true;
+});
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(x =>
+    {
+        x.LoginPath = "/";
+    });
+builder.Services.AddAuthorization();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddMemoryCache(); // Adds a default in-memory 
+                                   // implementation of 
+                                   // IDistributedCache
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(20);
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddTransient<CookieMiddleware>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -18,10 +57,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseSession();
+app.UseMiddleware<CookieMiddleware>();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
